a92db6aa43adb6b4dbdddf8521fc8f33487fe546
Ihar Bury <Ihar.Bury@gmail.com>
2010-09-23 00:01:28 +0000

    Illuminated Healing added Saving abilities added but not displayed yet

--- OrlanHeal.lua
+++ OrlanHeal.lua
@@ -118,6 +118,49 @@ function OrlanHeal:Initialize(configName)
 
 	self.RaidRoles = {};
 
+	self.SavingAbilities =
+	{
+		[498] = true, -- Divine Protection
+		[1022] = true, -- Hand of Protection
+		[5599] = true, -- Hand of Protection
+		[10278] = true, -- Hand of Protection
+		[20233] = true, -- Lay on Hands
+		[20236] = true, -- Lay on Hands
+		[642] = true, -- Divine Shield
+		[6940] = true, -- Hand of Sacrifice
+		[86657] = true, -- Ancient Guardian
+		[33206] = true, -- Pain Suppression
+		[47788] = true, -- Guardian Spirit
+		[47585] = true, -- Dispersion
+		[22812] = true, -- Barkskin
+		[22842] = true, -- Frenzied Regeneration
+		[61336] = true, -- Survival Instincts
+		[30823] = true, -- Shamanistic Rage
+		[48792] = true, -- Icebound Fortitude
+		[48707] = true, -- Anti-Magic Shell
+		[49222] = true, -- Bone Shield
+		[81256] = true, -- Dancing Rune Weapon
+		[50461] = true, -- Anti-Magic Zone
+		[19263] = true, -- Deterrence
+		[45438] = true, -- Ice Block
+		[5277] = true, -- Evasion
+		[74001] = true, -- Combat Readiness
+		[31224] = true, -- Cloak of Shadows
+		[45182] = true, -- Cheating Death
+		[59672] = true, -- Metamorphosis
+		[54373] = true, -- Nether Protection (Arcane)
+		[54371] = true, -- Nether Protection (Fire)
+		[54372] = true, -- Nether Protection (Frost)
+		[54370] = true, -- Nether Protection (Holy)
+		[54375] = true, -- Nether Protection (Nature)
+		[54374] = true, -- Nether Protection (Shadow)
+		[2565] = true, -- Shield Block
+		[871] = true, -- Shield Wall
+		[12975] = true, -- Last Stand
+		[46946] = true, -- Safeguard
+		[46947] = true -- Safeguard
+	};
+
 	self.HealingBuffs = {};
 	self.HealingBuffs[66922] = true; -- Вспышка Света
 	self.HealingBuffs[774] = true; -- Омоложение
@@ -196,6 +239,7 @@ function OrlanHeal:Initialize(configName)
 	self.HealingBuffs[59548] = true; -- Дар наару
 	self.HealingBuffs[59544] = true; -- Дар наару
 	self.HealingBuffs[59547] = true; -- Дар наару
+	self.HealingBuffs[86273] = true; -- Illuminated Healing
 
 	self.IgnoredDebuffs = {};
 	self.IgnoredDebuffs[58539] = true; -- Тело наблюдателя
