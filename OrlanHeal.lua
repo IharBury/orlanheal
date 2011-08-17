6489780d10b7475e8d72240e29a6a524e1dfda14
Ihar Bury <Ihar.Bury@gmail.com>
2011-08-17 21:45:14 +0000

    shaman

--- OrlanHeal.lua
+++ OrlanHeal.lua
@@ -2952,12 +2952,14 @@ function OrlanHeal.Shaman.CreateCooldowns(orlanHeal, cooldowns)
 	cooldowns[0] = orlanHeal:CreateCooldown(cooldowns.Frames[0], 0, 52127, 52127, true); -- Водный щит
 	cooldowns[1] = orlanHeal:CreateCooldown(cooldowns.Frames[0], 1, 974, 974, true); -- Щит земли
 	cooldowns[2] = orlanHeal:CreateCooldown(cooldowns.Frames[0], 2, 16188, 16188, false); -- Природная стремительность
+	cooldowns[3] = orlanHeal:CreateCooldown(cooldowns.Frames[0], 3, 26297, 26297, false); -- Берсерк
 end;
 
 function OrlanHeal.Shaman.UpdateCooldowns(orlanHeal)
 	orlanHeal:UpdatePlayerBuffCooldown(orlanHeal.RaidWindow.Cooldowns[0], 52127); -- Водный щит
 	orlanHeal:UpdateRaidBuffCooldown(orlanHeal.RaidWindow.Cooldowns[1], 974); -- Щит земли
 	orlanHeal:UpdateAbilityCooldown(orlanHeal.RaidWindow.Cooldowns[2], 16188); -- Природная стремительность
+	orlanHeal:UpdateAbilityCooldown(orlanHeal.RaidWindow.Cooldowns[3], 26297); -- Берсерк
 end;
 
 OrlanHeal.Shaman.RedRangeSpellId = 331; -- Волна исцеления
