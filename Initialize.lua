df2e6f1b5987bc10d3e96b347a934124951019a8
ihar <ihar@localhost>
2011-08-25 19:30:52 +0000

    decomposition

---
+++ Initialize.lua
@@ -0,0 +1,63 @@
+﻿function OrlanHeal:Initialize(configName)
+	local orlanHeal = self;
+
+	self.ConfigName = configName;
+	self.EventFrame = CreateFrame("Frame");
+
+	function self.EventFrame:HandleEvent(event, arg1)
+		if (event == "ADDON_LOADED") and (arg1 == "OrlanHeal") then
+			orlanHeal:HandleLoaded();
+		elseif (event == "RAID_ROSTER_UPDATE") or
+				(event == "PARTY_CONVERTED_TO_RAID") or
+				(event == "PARTY_LEADER_CHANGED") or
+				(event == "PARTY_MEMBERS_CHANGED") or
+				(event == "ZONE_CHANGED_NEW_AREA") or
+				(event == "PLAYER_REGEN_ENABLED") or
+				(event == "PLAYER_ENTERING_BATTLEGROUND") or
+				(event == "PLAYER_ENTERING_WORLD") or
+				(event == "LFG_ROLE_UPDATE") or
+				(event == "PLAYER_ROLES_ASSIGNED") or
+				(event == "ROLE_CHANGED_INFORM") then
+			if not InCombatLockdown() then
+				orlanHeal:UpdateUnits();
+			end;
+		end;
+	end;
+
+	local className, class = UnitClass("player");
+	if class == "PALADIN" then
+		self.Class = self.Paladin;
+	elseif class == "PRIEST" then
+		self.Class = self.Priest;
+	elseif class == "SHAMAN" then
+		self.Class = self.Shaman;
+	else
+		self.Class = self.Paladin;
+		print("OrlanHeal: " .. className .. " class is not supported.");
+	end;
+
+	self.ElapsedAfterUpdate = 0;
+	function self.EventFrame:HandleUpdate(elapsed)
+		if orlanHeal.RaidWindow:IsShown() then
+			orlanHeal.ElapsedAfterUpdate = orlanHeal.ElapsedAfterUpdate + elapsed;
+			if orlanHeal.ElapsedAfterUpdate > 1.0 / orlanHeal.FrameRate then
+				orlanHeal:UpdateStatus();
+				orlanHeal.ElapsedAfterUpdate = 0;
+			end;
+		end;
+	end;
+
+	self.EventFrame:RegisterEvent("ADDON_LOADED");
+	self.EventFrame:SetScript("OnEvent", self.EventFrame.HandleEvent);
+	self.EventFrame:SetScript("OnUpdate", self.EventFrame.HandleUpdate);
+
+	self.GroupCount = 9;
+	self.VisibleGroupCount = 9;
+	self.IsInStartUpMode = true;
+	self.IsTankWindowVisible = false;
+	self.IsNameBindingEnabled = false;
+
+	self.RaidRoles = {};
+end;
+
+OrlanHeal:Initialize("OrlanHealConfig");
---
+++ Priest.lua
@@ -0,0 +1,72 @@
+﻿OrlanHeal.Priest = {};
+
+OrlanHeal.Priest.AvailableSpells =
+{
+	1706, -- Левитация
+	527, -- Рассеивание заклинаний
+	17, -- Слово силы: Щит
+	6346, -- Защита от страха
+	2061, -- Быстрое исцеление
+	2060, -- Великое исцеление
+	2006, -- Воскрешение
+	528, -- Излечение болезни
+	2050, -- Исцеление
+	139, -- Обновление
+	32546, -- Связующее исцеление
+	33076, -- Молитва восстановления
+	73325, -- Духовное рвение
+	2096, -- Внутреннее зрение
+	47540, -- Исповедь
+	47788, -- Оберегающий дух
+	10060, -- Придание сил
+	33206, -- Подавление боли
+	59544, -- Дар Наару
+}
+
+function OrlanHeal.Priest.CreateCooldowns(orlanHeal, cooldowns)
+	cooldowns[0] = orlanHeal:CreateCooldown(cooldowns.Frames[0], 0, 89485, 89485, false); -- Внутреннее сосредоточение
+	cooldowns[1] = orlanHeal:CreateCooldown(cooldowns.Frames[0], 1, 17, 17, false); -- Слово силы: Щит
+end;
+
+function OrlanHeal.Priest.UpdateCooldowns(orlanHeal)
+	orlanHeal:UpdateAbilityCooldown(orlanHeal.RaidWindow.Cooldowns[0], 89485); -- Внутреннее сосредоточение
+	orlanHeal:UpdateAbilityCooldown(orlanHeal.RaidWindow.Cooldowns[1], 17); -- Слово силы: Щит
+end;
+
+OrlanHeal.Priest.RedRangeSpellId = 2096; -- Внутреннее зрение
+OrlanHeal.Priest.OrangeRangeSpellId = 1706; -- Левитация
+OrlanHeal.Priest.YellowRangeSpellId = 2061; -- Быстрое исцеление
+
+
+function OrlanHeal.Priest.UpdateRaidBorder(orlanHeal)
+	orlanHeal:SetBorderColor(orlanHeal.RaidWindow, 0, 0, 0, 0);
+end;
+
+OrlanHeal.Priest.PlayerSpecificBuffCount = 2;
+
+function OrlanHeal.Priest.GetSpecificBuffKind(orlanHeal, spellId, caster)
+	local buffKind;
+	if (spellId == 17) and (caster ~= nil) and (UnitIsUnit(caster, "player") == 1) then -- свой щит
+		buffKind = 1;
+	elseif (spellId == 139) and (caster ~= nil) and (UnitIsUnit(caster, "player") == 1) then -- своё восстановление
+		buffKind = 2;
+	end;
+
+	return buffKind;
+end;
+
+OrlanHeal.Priest.PoisonDebuffKind = 4;
+OrlanHeal.Priest.DiseaseDebuffKind = 2;
+OrlanHeal.Priest.MagicDebuffKind = 3;
+OrlanHeal.Priest.CurseDebuffKind = 4;
+OrlanHeal.Priest.PlayerDebuffSlots = { 1, 2, 3, 4, 0 };
+OrlanHeal.Priest.PetDebuffSlots = { 0, 0 };
+
+function OrlanHeal.Priest.GetSpecificDebuffKind(orlanHeal, spellId)
+	local debuffKind;
+	if spellId == 6788 then -- Ослабленная душа
+		debuffKind = 1;
+	end;
+
+	return debuffKind;
+end;
