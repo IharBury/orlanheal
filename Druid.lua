13459822c105e233f4bb626759b7ead2fc008c08
????? ????? <bury@itcd.ru>
2012-08-06 01:44:22 +0400

    Nature's Swiftness support for MoP added

--- Druid.lua
+++ Druid.lua
@@ -88,6 +88,42 @@ if GetBuildInfo() == "5.0.4" then
 	table.insert(OrlanHeal.Druid.AvailableSpells, 88423); -- Nature's Cure
 	table.insert(OrlanHeal.Druid.AvailableSpells, 110309); -- Symbiosis
 	table.insert(OrlanHeal.Druid.AvailableSpells, 102401); -- Wild Charge
+	table.insert(
+		OrlanHeal.Druid.AvailableSpells, 
+		{
+			type = "macro",
+			caption = "Instant " .. GetSpellInfo(8936), -- Восстановление
+			group = GetSpellInfo(132158), -- Природная стремительность
+			macrotext = OrlanHeal:BuildCastSequenceMacro(132158, 8936),
+			key = "17116,8936"
+		});
+	table.insert(
+		OrlanHeal.Druid.AvailableSpells, 
+		{
+			type = "macro",
+			caption = "Instant " .. GetSpellInfo(50464), -- Покровительство природы
+			group = GetSpellInfo(132158), -- Природная стремительность
+			macrotext = OrlanHeal:BuildCastSequenceMacro(132158, 50464),
+			key = "17116,50464"
+		});
+	table.insert(
+		OrlanHeal.Druid.AvailableSpells, 
+		{
+			type = "macro",
+			caption = "Instant " .. GetSpellInfo(20484), -- Возрождение
+			group = GetSpellInfo(132158), -- Природная стремительность
+			macrotext = OrlanHeal:BuildCastSequenceMacro(132158, 20484),
+			key = "17116,20484"
+		});
+	table.insert(
+		OrlanHeal.Druid.AvailableSpells, 
+		{
+			type = "macro",
+			caption = "Instant " .. GetSpellInfo(5185), -- Целительное прикосновение
+			group = GetSpellInfo(132158), -- Природная стремительность
+			macrotext = OrlanHeal:BuildCastSequenceMacro(132158, 5185),
+			key = "17116,5185"
+		});
 	OrlanHeal.Druid.CooldownOptions.Ironbark =
 	{
 		SpellId = 102342,
@@ -131,6 +167,11 @@ if GetBuildInfo() == "5.0.4" then
 	OrlanHeal.Druid.CooldownOptions.WildCharge =
 	{
 		SpellId = 102401,
+		Update = OrlanHeal.UpdateAbilityCooldown
+	};
+	OrlanHeal.Druid.CooldownOptions.NaturesSwiftness =
+	{
+		SpellId = 132158,
 		Update = OrlanHeal.UpdateAbilityCooldown
 	};
 else
